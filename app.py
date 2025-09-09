6ff00a08bcfc14743afa982d1a46865ff7886986
Sneha Saxena <162749533+snehasaxena685@users.noreply.github.com>
2025-09-09 15:08:30 +0530

    Update app.py

--- app.py
+++ app.py
@@ -202,4 +202,4 @@ if process_btn and uploaded:
         # -------------------------------
         if "Anaglyph" in preview_modes and anaglyph_frame is not None:
             st.subheader("👓 Anaglyph 3D Preview (Red/Cyan Glasses)")
-            st.image(anaglyph_frame, channels="BGR", caption="Preview with red/cyan glasses")
+            st.video(anaglyph_frame, channels="BGR", caption="Preview with red/cyan glasses")
